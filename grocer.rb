c59962a659e797ded951303757c1ca320536c19d
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-02 03:46:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,19 +37,13 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
-  {
-    :item => "#{c[:item]} W/COUPON",
-    :price => rounded_unit_price,
-    :count => c[:num]
-  }
+  
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  
 end
 
 def checkout(cart, coupons)
