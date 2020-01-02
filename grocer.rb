83044f67a3e89d52fe9c60e095e2eb79acf3be22
Jeemin Shim <jeemin.shim@gmail.com>
2020-01-02 03:52:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,20 +27,16 @@ def apply_coupons(cart, coupons)
   #
   # REMEMBER: This method **should** update cart
     coupons.each do |coupon|
-    if cart.keys.include? coupon[:item]
-      if cart[coupon[:item]][:count] >= coupon[:num]
-        new_name = "#{coupon[:item]} W/COUPON"
-        if cart[new_name]
-          cart[new_name][:count] += coupon[:num]
-        else
-          cart[new_name] = {
-            count: coupon[:num],
-            price: coupon[:cost]/coupon[:num],
-            clearance: cart[coupon[:item]][:clearance]
-          }
-        end
-        cart[coupon[:item]][:count] -= coupon[:num]
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
       end
+      cart[name][:count] -= coupon[:num]
     end
   end
   cart
